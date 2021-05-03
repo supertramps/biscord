2fa8f80825ac1cd22ff288be2bf508bc45dd5a12
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-03 11:42:47 +0200

    yay framework

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${}`)
+})

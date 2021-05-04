c1d3739b3782bf98c5673f6e1805f1bbc6a0f63f
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-04 13:52:58 +0200

    asdasd

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ io.on("connection", (socket) => {
     console.log("Client was connected", socket.id);
     io.emit('return-message', "Welcome!")
 
+    socket.on('join', (user) => {
+        console.log(user)
+    })
+
     /* socket.broadcast.emit('user-connected', socket.id)
     socket.emit('return-message', "Welcome!") */
 

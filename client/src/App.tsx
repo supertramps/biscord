c18aabb79a5729e9be5a074e20e31d8dd78ef652
supertramps <alexander.liljedahl@live.se>
2021-05-04 11:52:08 +0200

    Added Fonts, initial setup on mainPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import socketIOClient from "socket.io-client";
-import { ThemeProvider } from '@material-ui/core';
-import React from 'react'
+import { ThemeProvider } from "@material-ui/core";
 
-import './App.css';
-import LandingPage from './componants/LandingPage';
-import { theme } from './providers/ThemeProvider';
+import "./App.css";
+import LandingPage from "./componants/LandingPage";
+import { theme } from "./providers/ThemeProvider";
 import MainPage from "./componants/MainPage";
 
 function App() {
-  const socket = socketIOClient('http://localhost:6969', {
-    transports: ['websocket'],
+  const socket = socketIOClient("http://localhost:6969", {
+    transports: ["websocket"],
   });
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('connected');
+    socket.on("connect", () => {
+      console.log("connected");
     });
-    socket.on('event', (data: any) => {
+    socket.on("event", (data: any) => {
       console.log(data);
     });
-
   }, []);
   return (
-   <>
-    <MainPage />
-   <ThemeProvider theme={theme}>
-   <LandingPage/>
-   </ThemeProvider>
-   </>
-
+    <>
+      <MainPage />
+      <ThemeProvider theme={theme}>
+        <LandingPage />
+      </ThemeProvider>
+    </>
   );
-
 }
 
 export default App;

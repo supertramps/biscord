491f3cc084626af39bc1f49b596e93357b5c8702
Sebastian Zazzi <sebastian@zazzi.se>
2021-05-04 10:15:28 +0200

    basic layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import LandingPage from './componants/LandingPage';
 
 function App() {
   return (
    <>
-   <div>Hello world</div>
+   <LandingPage/>
    </>
   );
 }
--- client/src/componants/LandingPage.tsx
+++ client/src/componants/LandingPage.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import {
+  Box,
+  makeStyles,
+  TextField,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import logoGif from "../assets/biscord.gif"
 
 function LandingPage() {
-  return <Box></Box>;
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Typography variant="h1">Biscord</Typography>
+      <img src={logoGif} alt="logo.gif" />
+      <Typography>Like Discord, but with a B</Typography>
+      <form noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
+    </Box>
+  );
 }
 
 export default LandingPage;
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        display:"flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",   
+    }
+}));

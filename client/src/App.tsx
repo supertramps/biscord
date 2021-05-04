b133175ff34a9d211408e486feae081fb6e2bbdb
Sebastian Zazzi <sebastian@zazzi.se>
2021-05-04 11:06:11 +0200

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import socketIOClient from "socket.io-client";
-import { ThemeProvider } from '@material-ui/core';
-import React from 'react'
+import { ThemeProvider } from "@material-ui/core";
 
-import './App.css';
-import LandingPage from './componants/LandingPage';
-import { theme } from './providers/ThemeProvider';
+import "./App.css";
+import LandingPage from "./componants/LandingPage";
+import { theme } from "./providers/ThemeProvider";
 import MainPage from "./componants/MainPage";
+import { SocketConsumer } from "./providers/SocketContext";
 
 function App() {
-  const socket = socketIOClient('http://localhost:6969', {
-    transports: ['websocket'],
+  const socket = socketIOClient("http://localhost:6969", {
+    transports: ["websocket"],
   });
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('connected');
+    socket.on("connect", () => {
+      console.log("connected");
     });
-    socket.on('event', (data: any) => {
+    socket.on("event", (data: any) => {
       console.log(data);
     });
-
   }, []);
   return (
-   <>
-    <MainPage />
-   <ThemeProvider theme={theme}>
-   <LandingPage/>
-   </ThemeProvider>
-   </>
-
+    <>
+      {/* <SocketConsumer> */}
+        <MainPage />
+        <ThemeProvider theme={theme}>
+          <LandingPage />
+        </ThemeProvider>
+      {/* </SocketConsumer> */}
+    </>
   );
-
 }
 
 export default App;

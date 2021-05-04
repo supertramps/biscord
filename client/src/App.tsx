da668b2732d0a63b57539b33d5a87776f12d5be0
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-04 10:25:52 +0200

    werks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import socketIOClient from "socket.io-client";
 import './App.css';
 
 function App() {
+  const socket = socketIOClient('http://localhost:6969', {
+    transports: ['websocket'],
+  });
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected');
+    });
+    socket.on('event', (data: any) => {
+      console.log(data);
+    });
+
+  }, []);
   return (
-   <>
-   <div>Hello world</div>
-   </>
+    <div className='App'>
+     Socket Proxy test
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,50 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = process.env.PORT || 6969;
+const http = require('http')
+const { Server } = require('socket.io');
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${}`)
+const server = http.createServer(app);
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+    console.log("Client was connected", socket.id);
+    io.emit('return-message', "Welcome!")
+
+    /* socket.broadcast.emit('user-connected', socket.id)
+    socket.emit('return-message', "Welcome!") */
+
+    /* socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+
+    //all 
+    //io.emit('user-connected', socket.id)
+
+    // all but self
+    
+
+    // only to self 
+    socket.emit('return-message', "Welcome!")
+
+    socket.on('join-room', (data) => {
+        socket.join(data.room)
+
+        console.log("rooms:",io.sockets.adapter.rooms)
+        socket.to(data.room).emit('joined-room', `A user just joined the room ${socket.id}`)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Rooms: ', io.sockets.adapter.rooms)
+    }) */
+})
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", response);
+  };
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 })

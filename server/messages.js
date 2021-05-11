a4a2dd796754648c1506f9309641af9312b53d3f
Sebastian Zazzi <sebastian@zazzi.se>
2021-05-11 13:51:25 +0200

    now send to correct room and get/clears old messages

--- server/messages.js
+++ server/messages.js
@@ -5,7 +5,22 @@ function handleMessages(msg, user, room, time) {
   messages.push(message);
 }
 
+function filterMessages(room) {
+  const messagesInCurrentRoom = messages.filter(m => m.room === room);
+  return messagesInCurrentRoom;
+}
+
+function getMessages(room, session, io) {
+  const messagesInCurrentRoom = messages.filter(m => m.room === room);
+  io.to(room).emit("chat-message", {
+    messagesInCurrentRoom,
+    loggedInUser: session,
+  });
+}
+
 module.exports = {
   messages,
   handleMessages,
+  filterMessages,
+  getMessages
 };

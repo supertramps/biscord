5628bbc9583b1e9ed97f5140948e72ca2d527c27
supertramps <alexander.liljedahl@live.se>
2021-05-11 14:42:12 +0200

    Added chat commands to backend logic

--- server/messages.js
+++ server/messages.js
@@ -1,8 +1,30 @@
 const messages = [];
+const giphyRandom = require("giphy-random");
 
-function handleMessages(msg, user, room, time) {
-  const message = { message: msg, user, room, time };
-  messages.push(message);
+// Fetches a random GIF from Giphy API and returns a string with URL
+async function getGif(tag) {
+  const API_KEY = "nGgKX5djKNAVoYChgFHSzk7Q2tnOs65p";
+
+  const { data } = await giphyRandom(API_KEY, {
+    rating: "pg13",
+    tag: tag,
+  });
+  return data.embed_url;
+}
+
+async function handleMessages(msg, user, room, time) {
+  console.log(msg.substring(0, 4));
+  if (msg.substring(0, 4) === "/gif") {
+    const gifTag = msg.substring(5);
+    const randomGif = await getGif(gifTag);
+    console.log(randomGif);
+    let message = { message: randomGif, user, room, time };
+    messages.push(message);
+  } else {
+    let message = { message: msg, user, room, time };
+    messages.push(message);
+  }
+  console.log(messages);
 }
 
 module.exports = {

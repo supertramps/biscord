a064bb5e867a2d63a2c6146fb754a2dc68544cdd
supertramps <alexander.liljedahl@live.se>
2021-05-11 15:39:09 +0200

    Added /help command

--- server/messages.js
+++ server/messages.js
@@ -1,8 +1,8 @@
 const messages = [];
 
-const moment = require("moment")
+const moment = require("moment");
 const giphyRandom = require("giphy-random");
-  
+
 // Fetches a random GIF from Giphy API and returns a string with URL
 async function getGif(tag) {
   const API_KEY = "nGgKX5djKNAVoYChgFHSzk7Q2tnOs65p";
@@ -15,37 +15,53 @@ async function getGif(tag) {
 }
 
 async function handleMessages(msg, user, room, time) {
-  console.log(msg.substring(0, 4));
+  const helpMessage =
+    "Hi! There are some chat commands you can use on this server. Use `/gif` to post a random GIF. You may also search for a GIF using `/gif doge` (you are free to change doge to whatever you desire). Have fun 😎";
   if (msg.substring(0, 4) === "/gif") {
     const gifTag = msg.substring(5);
     const randomGif = await getGif(gifTag);
-    console.log(randomGif);
-    let message = { message: randomGif, user, room, time: moment().format("[Today at] HH:mm:ss") };
+    let message = {
+      message: randomGif,
+      user,
+      room,
+      time: moment().format("[Today at] HH:mm:ss"),
+    };
+    messages.push(message);
+  } else if (msg.substring(0, 5) === "/help") {
+    let message = {
+      message: helpMessage,
+      user,
+      room,
+      time: moment().format("[Today at] HH:mm:ss"),
+    };
     messages.push(message);
   } else {
-    const message = { message: msg, user, room, time: moment().format("[Today at] HH:mm:ss") };
+    const message = {
+      message: msg,
+      user,
+      room,
+      time: moment().format("[Today at] HH:mm:ss"),
+    };
     messages.push(message);
   }
-  console.log(messages);
-  }
+}
 
 function filterMessages(room) {
-  const messagesInCurrentRoom = messages.filter(m => m.room === room);
+  const messagesInCurrentRoom = messages.filter((m) => m.room === room);
   return messagesInCurrentRoom;
 }
 
 function getMessages(room, session, io) {
-  const messagesInCurrentRoom = messages.filter(m => m.room === room);
+  const messagesInCurrentRoom = messages.filter((m) => m.room === room);
   io.to(room).emit("chat-message", {
     messagesInCurrentRoom,
     loggedInUser: session,
   });
 }
 
-
 module.exports = {
   messages,
   handleMessages,
   filterMessages,
-  getMessages
+  getMessages,
 };
